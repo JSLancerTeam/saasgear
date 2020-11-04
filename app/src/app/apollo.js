6867c7655335197aadb5dcf8fab292529cbac1f1
Leo Phan <nhuthuy212507@gmail.com>
2020-11-04 18:36:41 +0700

    [Eslint] - update husky

--- app/src/app/apollo.js
+++ app/src/app/apollo.js
@@ -21,10 +21,12 @@ const client = new ApolloClient({
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors)
         graphQLErrors.forEach(({ message, locations, path }) =>
+          // eslint-disable-next-line no-console
           console.log(
             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
           ),
         );
+      // eslint-disable-next-line no-console
       if (networkError) console.log(`[Network error]: ${networkError}`);
 
       if (

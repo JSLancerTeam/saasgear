6306ea6cf47146c7b5d3b9e3f9dfe98962c6a606
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-11-04 11:10:00 +0700

    define structure backend

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+	type Book {
+		title: String
+		author: String
+	}
+	type Query {
+		books: [Book]
+	}
+`;
+
+const books = [
+	{
+		title: 'The Awakening',
+		author: 'Kate Chopin',
+	},
+	{
+		title: 'City of Glass',
+		author: 'Paul Auster',
+	},
+];
+
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
+});

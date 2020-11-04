5e6597c56a4767588a9b28f19d00639444a7f955
Leo Phan <nhuthuy212507@gmail.com>
2020-11-04 11:12:17 +0700

    [Structure] - structure basic for landing page

---
+++ landing/pages/index.js
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
+          Next.js + Tailwind CSS
+        </h1>
+      </div>
+    </div>
+  )
+}

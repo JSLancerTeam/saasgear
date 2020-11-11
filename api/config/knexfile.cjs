12391ab27ac824371cca2b3e5886da0eac11973f
Tuan Anh <tuananh@Davids-MacBook-Pro.local>
2020-11-11 15:33:13 +0700

    [JSL-8] Setup docker service for API

--- api/config/knexfile.cjs
+++ api/config/knexfile.cjs
@@ -1,13 +1,14 @@
 require('dotenv').config({ path: '../.env' });
 
-module.exports = (database, host, user, password) => ({
+module.exports = (database, host, user, password, port) => ({
 	development: {
 		client: 'mysql2',
 		connection: {
 			database: database ? database : process.env.DATABASE_NAME,
-			host: host ? host : process.env.DATABASE_HOST,
+			host: host ? host : process.env.DB_HOST_OUTSIZE_DOCKER,
 			user: user ? user : process.env.DATABASE_USER,
 			password: password ? password : process.env.DATABASE_PASSWORD,
+			port: port || process.env.DB_PORT_OUTSIZE_DOCKER
 		},
 		migrations: {
 			tableName: 'migrations',

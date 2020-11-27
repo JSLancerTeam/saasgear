276d075d8f7882275de3062ce209bc87fb6a8854
davidtran <nam.trankhanh.vn@gmail.com>
2020-11-27 15:04:50 +0700

    updat email templates folder

--- api/helpers/generate-template-email.js
+++ api/helpers/generate-template-email.js
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 import path from 'path';
 
 export default function generateEmailTemplate({ fileName, data }) {
-  const templatePath = `${path.resolve()}/email-template/${fileName}`;
+  const templatePath = `${path.resolve()}/email-templates/${fileName}`;
   const template = execSync(`mjml ${templatePath}`).toString();
   return handlebars.compile(template)(data).toString();
 }

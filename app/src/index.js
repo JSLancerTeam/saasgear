18668fe574d8a9bd85a5136561e89acf226df230
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-11-30 11:45:05 +0700

    add sentry

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 import store from '@/config/store';
 import App from './App';
 
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 const render = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <Sentry.ErrorBoundary fallback="An error has occurred">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Sentry.ErrorBoundary>,
     document.getElementById('root'),
   );
 };

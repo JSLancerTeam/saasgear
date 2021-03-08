ee2c603cf44c11cedbe79db1f08a3107f43bde43
Leo Phan <nhuthuy212507@gmail.com>
2021-03-08 18:03:44 +0700

    update style accept invitation

--- app/src/containers/Teams/AcceptInvitation.jsx
+++ app/src/containers/Teams/AcceptInvitation.jsx
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { useParams, useHistory } from "react-router-dom";
 import { useQuery, useLazyQuery, useMutation } from "@apollo/react-hooks";
 import verifyTokenQuery from "@/queries/teams/verifyInviteToken";
 import getProfileQuery from "@/queries/auth/getProfile";
 import joinTeamQuery from "@/queries/teams/joinTeam";
-import logo from '@/assets/images/logo.png';
+import Logo from '@/components/Common/Logo';
+import { 
+  ForgotPasswordWrapper, 
+  Overlay, 
+  ForgotPasswordContainer, 
+  ForgotPasswordText, 
+  ForgotPasswordDescription, 
+  SquareIconTop, 
+  SmallSquareBottom, 
+  SmallSquareTop, 
+  SmallSquareGrid, 
+  SquareIconBottom, 
+  CircleIcon,
+} from '@/components/Auth/AuthForm';
+import Button from '@/components/Common/Button/Button';
 
-AcceptInvitation.propTypes = {
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
 
-};
+  button:first-child {
+    margin-right: 32px;
+  }
+`;
 
 function AcceptInvitation() {
   const { invitationToken } = useParams()
@@ -46,33 +66,61 @@ function AcceptInvitation() {
 
   return loading && getProfileLoading ?
     <div> Loading ....</div>
-    : <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <div className="flex items-center">
-        <img className="h-48 w-auto" src={logo} alt="JSlancer" />
-        <div className="p-10">
-          <div className="text-3xl">Accept Invitation?</div>
-          <div className="text-gray-400 mb-4">
+    : (
+      <ForgotPasswordWrapper>
+        <Overlay />
+        <ForgotPasswordContainer>
+          <div>
+            <Logo />
+          </div>
+          <ForgotPasswordText>
+            Accept Invitation?
+          </ForgotPasswordText>
+          <ForgotPasswordDescription>
             You&apos; ve been invitated to join <strong>{teamInfo?.teamName}</strong> by <strong>{teamInfo?.owner}</strong>
-          </div>
-          <div className="flex">
-            <button
-              type="button"
-              onClick={() => handleUserJoinTeam('accept')}
-              className="group flex items-center justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-            >
-              Accept
-            </button>
-            <button
-              type="button"
-              onClick={() => handleUserJoinTeam('decline')}
-              className="group flex items-center justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-indigo active:bg-red-700 transition duration-150 ease-in-out ml-2"
-            >
-              Decline
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+          </ForgotPasswordDescription>
+          <ButtonGroup>
+            <Button color="primary" onClick={() => handleUserJoinTeam('accept')}>Accept</Button>
+            <Button onClick={() => handleUserJoinTeam('decline')}>Decline</Button>
+          </ButtonGroup>
+          <SquareIconTop>
+            <svg width="496" height="482" viewBox="0 0 496 482" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 1H445C472.614 1 495 23.3858 495 51V481H50C22.3858 481 0 458.614 0 431V1Z" stroke="#2291FF"/>
+            </svg>
+          </SquareIconTop>
+          <SmallSquareBottom>
+            <svg width="195" height="195" viewBox="0 0 195 195" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0H145C172.614 0 195 22.3858 195 50V195H50C22.3858 195 0 172.614 0 145V0Z" fill="#0075E8"/>
+            </svg>
+          </SmallSquareBottom>
+          <SmallSquareTop>
+            <svg width="114" height="121" viewBox="0 0 114 121" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0H64C91.6142 0 114 22.3858 114 50V121H50C22.3858 121 0 98.6142 0 71V0Z" fill="#1788F8"/>
+            </svg>
+          </SmallSquareTop>
+          <SmallSquareGrid>
+            <svg width="131" height="123" viewBox="0 0 131 123" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g opacity="0.5">
+                <path d="M129.844 121.5H1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="1 40"/>
+                <path d="M129.844 81.5H1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="1 40"/>
+                <path d="M129.844 41.5H1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="1 40"/>
+                <path d="M129.844 1.5H1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="1 40"/>
+              </g>
+            </svg>
+          </SmallSquareGrid>
+          <SquareIconBottom>
+            <svg width="594" height="523" viewBox="0 0 594 523" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 1H544C571.614 1 594 23.3858 594 51V529H51C23.3858 529 1 506.614 1 479V1Z" stroke="#2291FF"/>
+            </svg>
+          </SquareIconBottom>
+          <CircleIcon>
+            <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="30" cy="30" r="29" stroke="#2291FF"/>
+            </svg>
+          </CircleIcon>
+        </ForgotPasswordContainer>
+      </ForgotPasswordWrapper>
+    )
 }
 
 export default AcceptInvitation;

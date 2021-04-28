d74b29dccdf63729d319250a5ae69d4a2daca765
Clement <tuananhit.oct@gmail.com>
2021-04-28 11:49:39 +0700

    Revert "Init db and command generate tool"
    This reverts commit 49c004967fa3b2e478ede0fef6a0395fe997cc52.

---
+++ api/services/stripe/subcription.service.js
@@ -0,0 +1,95 @@
+import Apollo from 'apollo-server-express';
+import dayjs from 'dayjs';
+import Stripe from 'stripe';
+import logger from '~/utils/logger';
+import { normalizeEmail } from '~/helpers/string.helper';
+
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
+const { ApolloError } = Apollo;
+
+/**
+ * Create new subcription
+ *
+ * @param {string} token
+ * @param {string} email
+ * @param {string} name
+ * @param {string} priceId
+ * @param {boolean} isTrial
+ *
+ * @returns {Promise<any>}
+ */
+export async function createNewSubcription(token, email, name, priceId, isTrial = false) {
+  if (!token) {
+    throw new ApolloError('Invalid token');
+  }
+
+  try {
+    const customer = await stripe.customers.create({
+      email: normalizeEmail(email),
+      name,
+      source: token,
+    });
+
+    const dataSubcription = {
+      customer: customer.id,
+      items: [{ price: priceId }],
+    };
+    if (isTrial) {
+      dataSubcription.trial_end = dayjs().add(14, 'd').unix();
+    }
+
+    const result = await stripe.subscriptions.create(dataSubcription);
+
+    return {
+      customer_id: customer.id,
+      subcription_id: result.id,
+    };
+  } catch (error) {
+    logger.error(error);
+    throw new ApolloError('Payment failed! Please check your card.');
+  }
+}
+
+/**
+ * Update subcription
+ *
+ * @param {string} subId
+ * @param {string} priceId
+ *
+ * @returns {Promise<any>}
+ */
+export async function updateSubcription(subId, priceId) {
+  try {
+    const subscription = await stripe.subscriptions.retrieve(subId);
+
+    await stripe.subscriptions.update(subId, {
+      items: [{
+        id: subscription.items.data[0].id,
+        price: priceId,
+      }],
+    });
+
+    return true;
+  } catch (error) {
+    logger.error(error);
+    throw new ApolloError('Payment failed! Please check your card.');
+  }
+}
+
+/**
+ * Cancel subcription
+ *
+ * @param {string} customerId
+ *
+ * @returns {Promise<any>}
+ */
+export async function cancelSubcription(customerId) {
+  try {
+    await stripe.customers.del(customerId);
+
+    return true;
+  } catch (error) {
+    logger.error(error);
+    throw new ApolloError('Something went wrong!');
+  }
+}
